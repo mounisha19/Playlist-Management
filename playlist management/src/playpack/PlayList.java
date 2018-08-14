2e22b3c8f5c406f4fce2b48ec66d14124f23cb71
mouni <mouni@LAPTOP-F36MH9C5.mshome.net>
2018-08-14 11:49:10 +0530

    Commit First

---
+++ playlist management/src/playpack/PlayList.java
@@ -0,0 +1,35 @@
+package playpack;
+
+
+import java.util.*;
+public class PlayList {
+
+	String name;
+	int id;
+	ArrayList<Song> slist =new ArrayList<Song>();
+	void setName(String name)
+	{
+		this.name=name;
+	}
+	void setId(int id)
+	{
+		this.id=id;
+	}
+	void setList(ArrayList<Song> slist)
+	{
+	this.slist=slist;
+	}
+	ArrayList<Song> getList()
+	{
+		return slist;
+	}
+	String getName()
+	{
+		return name;
+	}
+	int getId()
+	{
+		return id;
+	}
+	
+}
